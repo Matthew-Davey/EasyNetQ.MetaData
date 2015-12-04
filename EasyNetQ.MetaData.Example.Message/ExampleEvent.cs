a4502f206d89b66bfd8c022aa45fb77334589ecb
Matt Davey <matt.davey@fsfe.org>
2015-12-04 22:39:58 +0000

    Adds example message producer

---
+++ EasyNetQ.MetaData.Example.Message/ExampleEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EasyNetQ.MetaData.Example.Message {
+    public class ExampleEvent {
+        public String MessageContent { get; set; }
+
+        [MessageHeader("header_value")]
+        public String HeaderValue { get; set; }
+    }
+}

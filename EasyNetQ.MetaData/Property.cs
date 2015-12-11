4dbdbe4f044e5aab4677e976a430c8543d8965e5
Matt Davey <romeblue@gmail.com>
2015-12-11 22:38:20 +0000

    Adds enumeration of AMQP message property types and attribute to decorate properties

---
+++ EasyNetQ.MetaData/Property.cs
@@ -0,0 +1,17 @@
+﻿namespace EasyNetQ.MetaData {
+    public enum Property {
+        ContentType,
+        ContentEncoding,
+        DeliveryMode,
+        Priority,
+        CorrelationId,
+        ReplyTo,
+        Expiration,
+        MessageId,
+        Timestamp,
+        Type,
+        UserId,
+        AppId,
+        ClusterId,
+    }
+}

a2ef08c69423d3a5fd0224e9b83df9e351e63f65
Matt Davey <matt.davey@fsfe.org>
2017-06-01 22:51:57 +0100

    Add xml comments for public facing API

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.4.0")]
-[assembly: AssemblyFileVersion("0.4.0.5c88af")]
+[assembly: AssemblyFileVersion("0.4.0.de849a")]
 [assembly: AssemblyInformationalVersion("0.4.0")]
